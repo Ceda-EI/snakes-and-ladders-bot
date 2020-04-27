a3b72c5644cbf87959a8f612d203ce53e325b187
Ceda EI <ceda_ei@webionite.com>
2020-04-27 13:48:06 +0530

    Add dice handler. Send color in /join.

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@
 import logging
 import random
 
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 import config
 
 from boards import BOARDS
-from sal import Board
+from sal import Board, NotTurnError
 
 
 def start(update, context):
@@ -45,11 +45,13 @@ def join(update, context):
 
     game = context.chat_data["game"]
     first_name = update.effective_user.first_name
-    game.add_player(update.effective_user.id, first_name)
-    context.bot.send_message(update.effective_chat.id, f"{first_name} joined!")
+    color = game.add_player(update.effective_user.id, first_name)
+    message = f"{first_name} joined with {color.name} color!"
+    context.bot.send_message(update.effective_chat.id, message)
 
 
 def begin(update, context):
+    "/begin"
     if "game" not in context.chat_data:
         context.bot.send_message(update.effective_chat.id,
                                  "No game in progress.")
@@ -62,6 +64,50 @@ def begin(update, context):
     context.bot.send_message(update.effective_chat.id, message)
 
 
+def dice(update, context):
+    "Handles dice being thrown"
+    if "game" not in context.chat_data:
+        context.bot.send_message(update.effective_chat.id,
+                                 "No game in progress.")
+        return
+
+    if not context.chat_data.get("begin", False):
+        context.bot.send_message(update.effective_chat.id,
+                                 "Game has not started yet.")
+        return
+
+    game = context.chat_data["game"]
+    pid = update.effective_user.id
+
+    if pid != game.turn["pid"]:
+        return
+
+    steps = update.message.dice.value
+    try:
+        player = game.turn
+        final_position, direction = game.move(pid, steps, check_turn=True)
+    except NotTurnError:
+        return
+    img = game.draw()
+    if final_position == 100:
+        message = f"{player['name']} ({player['color'].name}) won! Game ended."
+        del context.chat_data["game"]
+        context.chat_data["begin"] = False
+    elif direction == 1:
+        message = (f"{player['name']} grabbed a ladder and reached "
+                   f"{final_position}.")
+    elif direction == 0:
+        message = f"{player['name']} reached {final_position}."
+    elif direction == -1:
+        message = (f"{player['name']} was dragged down by a snake and reached "
+                   f"{final_position}.")
+
+    if final_position != 100:
+        next_player = game.turn
+        message += f" Current turn: {next_player['name']}"
+    context.bot.send_photo(update.effective_chat.id, img, caption=message)
+
+
 def main():
     "Main function"
     logging.basicConfig(level=logging.INFO)
@@ -71,10 +117,12 @@ def main():
     newgame_handler = CommandHandler("newgame", newgame)
     join_handler = CommandHandler("join", join)
     begin_handler = CommandHandler("begin", begin)
+    dice_handler = MessageHandler(filters.Filters.dice, dice)
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(newgame_handler)
     dispatcher.add_handler(join_handler)
     dispatcher.add_handler(begin_handler)
+    dispatcher.add_handler(dice_handler)
     updater.start_polling()
 
 if __name__ == "__main__":
